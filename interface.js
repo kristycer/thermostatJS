d296a885866cab0acfa2e6f7edd22f964b6f1e90
kristyna cervenkova <kristyna275@gmail.com>
2020-10-04 18:29:55 +0200

    interface

--- interface.js
+++ interface.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   var thermostat = new Thermostat();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=c802e2ca7afd6c86c4403868b10d9005=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+})
+
   updateTemperature();
 
   $('#temperature-up').click(function () {
